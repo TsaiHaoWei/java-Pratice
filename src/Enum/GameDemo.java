eade189d971e4ec166fa1aba2e3cd76f73d435c5
Tsai <tsai@gmail.com>
2018-11-26 21:31:11 +0800

    11_26 Finally

--- src/Enum/GameDemo.java
+++ src/Enum/GameDemo.java
@@ -4,8 +4,30 @@ public class GameDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Game.play(Action.DOWN.ordinal());
-		Game.play(Action.UP.ordinal());
+		GameDemo.play(Action.DOWN.ordinal());
+		GameDemo.play(Action.UP.ordinal());
+		
+	}
+
+	private static void play(int ordinal) {
+		// TODO Auto-generated method stub
+		switch(ordinal){
+			case 0:
+				System.out.println("Stop");
+				break;
+			case 1:
+				System.out.println("Right");
+				break;
+			case 2:
+				System.out.println("Left");
+				break;
+			case 3:
+				System.out.println("Up");
+				break;
+			case 4:
+				System.out.println("Down");
+				break;
+		}
 	}
 
 }
