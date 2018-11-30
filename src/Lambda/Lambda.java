f3b20185cc022917bacad0048d94a7e9808d44fa
Tsai <tsai@gmail.com>
2018-11-30 21:46:21 +0800

    2018/11/30_finally

--- src/Lambda/Lambda.java
+++ src/Lambda/Lambda.java
@@ -1,12 +1,19 @@
 package Lambda;
 
 import java.util.Arrays;
+import java.util.Comparator;
+
+import java.util.*;
+
 
 public class Lambda {
 	public static void main (String[] args) 
-	{
+	{	
+		/*Comparator<String> lengthComparator = StringOrder::byLength;
 		String[] names = {"Justin", "caterpillar", "Bush"};
-		Arrays.sort(names, StringOrder::byLength);
+	//	Arrays.sort(names, StringOrder::byLength);
+		
+		
 		for(String name:names) System.out.printf("%s ",name);
 			System.out.println();
 		
@@ -16,6 +23,12 @@ public class Lambda {
 		
 		Arrays.sort(names, StringOrder::byLexicographylgnaoreCase);
 		StringOrder.Stringprint(names);
+		*/
+		List<String> names = Arrays.asList("Justin", "Monica", "Irene");
+		names.forEach(name -> System.out.println(name));
+		new HashSet(names).forEach(name -> System.out.println(name));
+		new ArrayDeque(names).forEach(name -> System.out.println(name));
+		
 	}
 }
 //trim(); 是從資料庫取值時 IF____ 比較長度時空白一樣會被計算
